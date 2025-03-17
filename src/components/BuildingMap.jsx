7c184d311b56700f0f83333e22294310572765de
sadtowa <toni.afanasev.0505@mail.ru>
2025-03-17 22:07:26 +0500

    zoom

--- src/components/BuildingMap.jsx
+++ src/components/BuildingMap.jsx
@@ -1,50 +1,164 @@
 import {Layer, Path, Rect, Stage, Text} from "react-konva";
-import {useEffect, useState} from "react";
+import {use, useEffect, useMemo, useRef, useState} from "react";
 
 function BuildingMap() {
-  const [stage, setStage] = useState({
-    scale: 0.3,
-    x: 300,
-    y: 300
-  });
+  const [stageScale, setStageScale] = useState(0.5);
+  const [stageX, setStageX] = useState(0);
+  const [stageY, setStageY] = useState(0);
 
+  const [isZooming, setIsZooming] = useState(false);
   const [coords, setCoords] = useState({})
   const [curLayer, setCurLayer] = useState(0)
+
+  function getCenter(p1, p2) {
+    return {
+      x: (p1.x + p2.x) / 2,
+      y: (p1.y + p2.y) / 2,
+    };
+  }
+
+  const lastCenterRef = useRef(null);
+  const lastDistRef = useRef(0);
+
+  const handleMultiTouch = (e) => {
+    e.evt.preventDefault();
+
+    var touch1 = e.evt.touches[0];
+    var touch2 = e.evt.touches[1];
+    const stage = e.target.getStage();
+
+    if (touch1 && touch2) {
+      setIsZooming(true);
+
+      const p1 = {
+        x: touch1.clientX,
+        y: touch1.clientY,
+      };
+      const p2 = {
+        x: touch2.clientX,
+        y: touch2.clientY,
+      };
+
+      if (!lastCenterRef.current) {
+        lastCenterRef.current = getCenter(p1, p2);
+        return;
+      }
+      const newCenter = getCenter(p1, p2);
+
+      function getDistance(p1, p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+      }
+
+      const dist = getDistance(p1, p2);
+
+      if (!lastDistRef.current) {
+        lastDistRef.current = dist;
+      }
+
+      // local coordinates of center point
+      var pointTo = {
+        x: (newCenter.x - stage.x()) / stage.scaleX(),
+        y: (newCenter.y - stage.y()) / stage.scaleX(),
+      };
+
+      var scale = stage.scaleX() * (dist / lastDistRef.current);
+
+      stage.scaleX(scale);
+      stage.scaleY(scale);
+
+      // calculate new position of the stage
+      const dx = newCenter.x - lastCenterRef.current.x;
+      const dy = newCenter.y - lastCenterRef.current.y;
+
+      const newPos = {
+        x: newCenter.x - pointTo.x * scale + dx,
+        y: newCenter.y - pointTo.y * scale + dy,
+      };
+
+      stage.position(newPos);
+      stage.batchDraw();
+
+      lastDistRef.current = dist;
+      lastCenterRef.current = newCenter;
+    }
+  };
+
+  const multiTouchEnd = () => {
+    lastCenterRef.current = null;
+    lastDistRef.current = 0;
+    setIsZooming(false);
+  };
 
   const handleWheel = (e) => {
     e.evt.preventDefault();
 
-    const scaleBy = 1.02;
+    const scaleBy = 1.2;
     const stage = e.target.getStage();
     const oldScale = stage.scaleX();
     const mousePointTo = {
       x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
-      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale,
     };
 
-    const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    const newScale =
+      e.evt.deltaY > 0
+        ? oldScale > 3
+          ? oldScale
+          : oldScale * scaleBy
+        : oldScale < 0.2
+          ? oldScale
+          : oldScale / scaleBy;
 
-    setStage({
-      scale: newScale,
-      x: (stage.getPointerPosition().x / newScale - mousePointTo.x) * newScale,
-      y: (stage.getPointerPosition().y / newScale - mousePointTo.y) * newScale
-    });
+    setStageScale(newScale);
+    setStageX(
+      -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale
+    );
+    setStageY(
+      -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale
+    );
   };
 
-  const [layers, setLayers] = useState({});
+  const [layers, setLayers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/map_data").then((response) => {
+    fetch("https://staticstorm.ru/map/map_data").then((response) => {
         response.json().then(
           (response) => {
-            setLayers(response)
+            setLayers([...response.buildings])
             setLoading(false)
           }
         )
       }
     );
   }, []);
+  const handleDragStart = (e) => {
+    const stage = e.target.getStage();
+
+    if (isZooming) {
+      stage.stopDrag();
+    }
+
+    console.log(stage.isDragging());
+  };
+
+  const renderedLayers = useMemo(() => (
+    layers[curLayer]?.floors.map(floor => (
+      floor.rooms.map(room => (
+        <Rect key={room.id}
+              x={room.x}
+              y={room.y}
+              fill="white"
+              width={room.width}
+              height={room.height}
+              stroke="black"
+              strokeWidth={1}
+              onClick={() => alert(room.id)}
+        />
+      ))
+    ))
+  ), [curLayer, layers]);
+
 
   if (loading) {
     return <div>Loading...</div>;
@@ -58,58 +172,23 @@ function BuildingMap() {
     }
   }
 
-  const moveHandler = (e) => {
-    setCoords({x: e.evt.clientX, y: e.evt.clientY})
-  }
-
   return (
     <>
       <button onClick={clickHandler}>change layer</button>
-      <div>X:{coords.x}, Y:{coords.y}</div>
       <Stage height={window.innerHeight}
              width={window.innerWidth}
              onWheel={handleWheel}
-             onMouseMove={moveHandler}
-             scaleX={stage.scale}
-             scaleY={stage.scale}
-             x={stage.x}
-             y={stage.y}
+             onTouchMove={handleMultiTouch}
+             onTouchEnd={multiTouchEnd}
+             onDragStart={handleDragStart}
+             scaleX={stageScale}
+             scaleY={stageScale}
+             x={stageX}
+             y={stageY}
              draggable
       >
         <Layer>
-          {layers.buildings[curLayer].floors.map((floor) => (
-            floor.paths.map((path) => (
-              <Path
-                data={path.d}
-                key={path.id}
-                stroke={path.stroke}
-                strokeWidth={path.strokeWidth}
-              >
-              </Path>
-            ))
-          ))}
-
-          {layers.buildings[curLayer].floors.map((floor) => (
-              floor.rooms.map(room => (
-                  <Rect
-                    key={room.id}
-                    x={room.x}
-                    y={room.y}
-                    width={room.width}
-                    height={room.height}
-                    stroke={"black"}
-                    strokeWidth={1}
-                    onClick={() => {
-                      alert(room.id)
-                    }}
-                  >
-                  </Rect>
-                )
-              )
-            )
-          )}
-
-
+          {renderedLayers}
         </Layer>
       </Stage>
     </>

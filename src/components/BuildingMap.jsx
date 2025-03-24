b2434897aadb0463e5a1950231f4e9d065b1aa92
sadtowa <toni.afanasev.0505@mail.ru>
2025-03-24 23:10:06 +0500

    merged by hands

--- src/components/BuildingMap.jsx
+++ src/components/BuildingMap.jsx
@@ -1,17 +1,16 @@
 import {Layer, Path, Rect, Stage, Text} from "react-konva";
-import {useEffect, useMemo, useRef, useState} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import RoomInfoModal from "./RoomInfoModal.jsx";
 import '../BuildingMap.css'
 import useStore from './store.jsx';
 
 
-function BuildingMap({ isMapActive }) {
+function BuildingMap({isMapActive}) {
   const [stageScale, setStageScale] = useState(0.5);
   const [stageX, setStageX] = useState(0);
   const [stageY, setStageY] = useState(0);
 
   const [isZooming, setIsZooming] = useState(false);
-  const [coords, setCoords] = useState({})
   const [curLayer, setCurLayer] = useState(0)
 
   function getCenter(p1, p2) {
@@ -33,7 +32,7 @@ function BuildingMap({ isMapActive }) {
     const stage = e.target.getStage();
 
     if (touch1 && touch2) {
-        stage.stopDrag();
+      stage.stopDrag();
       setIsZooming(true);
 
       const p1 = {
@@ -45,11 +44,11 @@ function BuildingMap({ isMapActive }) {
         y: touch2.clientY,
       };
 
-        if (!lastCenterRef.current || !lastDistRef.current) {
-            lastCenterRef.current = getCenter(p1, p2);
-            lastDistRef.current = getDistance(p1, p2);
-            return;
-        }
+      if (!lastCenterRef.current || !lastDistRef.current) {
+        lastCenterRef.current = getCenter(p1, p2);
+        lastDistRef.current = getDistance(p1, p2);
+        return;
+      }
       const newCenter = getCenter(p1, p2);
 
       function getDistance(p1, p2) {
@@ -132,18 +131,15 @@ function BuildingMap({ isMapActive }) {
 
 
   useEffect(() => {
-    fetch("https://staticstorm.ru/map/map_data").then((response) => {
-      response.json().then((response) => {
-        setLayers([...response.buildings]);
-        setLoading(false);
-
-        // Сохраняем все кабинеты в глобальное состояние
-        const allRooms = response.buildings.flatMap(building =>
-            building.floors.flatMap(floor => floor.rooms)
-        );
-        useStore.getState().setRooms(allRooms);
-      });
-    });
+    fetch("https://staticstorm.ru/map/map_data2").then((response) => {
+        response.json().then(
+          (response) => {
+            setLayers(response.layers)
+            setLoading(false)
+          }
+        )
+      }
+    );
   }, []);
 
   const handleDragStart = (e) => {
@@ -174,41 +170,89 @@ function BuildingMap({ isMapActive }) {
     setCurLayer(layerIndex);
   };
 
-  const renderedLayers = useMemo(() => (
-    layers[curLayer]?.floors.map(floor => (
-      floor.rooms.map(room => (
-        <Rect key={room.id}
-              x={room.x}
-              y={room.y}
-              fill="white"
-              width={room.width}
-              height={room.height}
-              stroke="black"
-              strokeWidth={1}
-              onClick={() => handleRoomClick(room)}
-              onTap={(e) => handleTouchRoom(e, room)}
-        />
-      ))
+  const renderedWalls = useMemo(() =>
+    (layers[curLayer]?.walls.map(wall => (
+      <Path
+        data={wall.data}
+        stroke={"black"}
+      />
+    ))),
+  )
+
+  const renderedIcons = useMemo(() => (
+    layers[curLayer]?.vectors.map((vector) => (
+      <Path
+        data={vector.data}
+        stroke={"black"}
+        strokeWidth={1}
+      />
     ))
+  ))
+
+  const renderedRooms = useMemo(() => (
+    layers[curLayer]?.rooms.map(room => {
+      if (room.x === undefined) {
+        return (
+
+          <Path
+            key={room.id}
+            id={room.id}
+            data={room.data}
+            stroke={"black"}
+            strokeWidth={1}
+            onClick={() => handleRoomClick(room)}
+            onTap={(e) => handleTouchRoom(e, room)}
+          />
+
+        )
+      }
+      return (
+        <React.Fragment key={room.id}>
+          <Rect
+            id={room.id}
+            x={room.x}
+            y={room.y}
+            width={room.width}
+            height={room.height}
+            stroke="black"
+            strokeWidth={1}
+            onTouchStart={() => alert(room.id)}
+            onClick={() => handleRoomClick(room)}
+            onTap={(e) => handleTouchRoom(e, room)}
+          />
+          <Text
+            x={room.x + room.width / 2}
+            y={room.y + room.height / 2}
+            offsetX={room.width / 4}
+            offsetY={7}
+            text={room.id}
+            fontSize={14}
+            fill="black"
+          />
+        </React.Fragment>
+      );
+    })
+
   ), [curLayer, layers]);
+
 
 
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+
 
   return (
     <>
       <div className="floor-buttons">
         {[4, 0, 1, 2, 3].map((layerIndex) => (
-            <button
-                key={layerIndex}
-                className={`floor-button ${curLayer === layerIndex ? 'active' : ''}`}
-                onClick={() => handleLayerChange(layerIndex)}
-            >
-              {layerIndex === 4 ? '0' : `${layerIndex + 1}`}
-            </button>
+          <button
+            key={layerIndex}
+            className={`floor-button ${curLayer === layerIndex ? 'active' : ''}`}
+            onClick={() => handleLayerChange(layerIndex)}
+          >
+            {layerIndex === 4 ? '0' : `${layerIndex + 1}`}
+          </button>
         ))}
       </div>
       <Stage height={window.innerHeight}
@@ -224,10 +268,12 @@ function BuildingMap({ isMapActive }) {
              draggable
       >
         <Layer>
-          {renderedLayers}
+          {renderedWalls}
+          {renderedRooms}
+          {renderedIcons}
         </Layer>
       </Stage>
-      <RoomInfoModal room={selectedRoom} onClose={() => setSelectedRoom(null)} />
+      <RoomInfoModal room={selectedRoom} onClose={() => setSelectedRoom(null)}/>
     </>
   );
 

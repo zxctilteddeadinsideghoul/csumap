6cdb7b54a657e18fb66832ec6072e980bc2553a2
sadtowa <toni.afanasev.0505@mail.ru>
2025-03-25 00:25:14 +0500

    )

--- src/components/MapRedactor.jsx
+++ src/components/MapRedactor.jsx
@@ -121,7 +121,7 @@ function MapRedactor() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/save", {
+      const response = await fetch("https://staticstorm.ru/map/save", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
